ded89cc37e1998697926e5462c876f6ed098572f
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-10-14 14:29:03 +0800

    build(deps): add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
