5ce659bde62fd5362e5f9992aa57c8930b53e404
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-10-15 15:47:58 +0800

    build(deps): add lang

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -8,4 +8,5 @@ return [
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    LaravelLang\JsonFallback\TranslationServiceProvider::class,
 ];
